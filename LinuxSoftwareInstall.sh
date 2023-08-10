83eec8680b549156ba3fe6c3b0ffec9002ee6423
CYBER_KARTHIK <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-08-10 22:28:06 +0530

    Update LinuxSoftwareInstall.sh

--- LinuxSoftwareInstall.sh
+++ LinuxSoftwareInstall.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 
 
-Flutter_Install(){
-	
-	sudo snap install flutter --classic  -y
-	flutter sdk-path
-	flutter doctor
-	read -p "Do you want to Setup Flutter Path? (y/n): " flutter_path
-	if [[ $flutter_path == "y" ]]; then
-		export PATH="$PATH:/snap/bin/flutter/bin"
-		echo -e "\n\n Flutter Path Setup Competed......\n\n"
-	fi
-	echo -e "\n\n Flutter Install Competed......\n\n"
-}
-
-
-
-
-
 while true; do
     echo -e " \n\n\t\t\t Welcome to Software Installer\n\t 1. Update System\n\t 2. Media Softwares VLC\n\t 3. Programming Software Python,GCC,GIT\n\t 4. Code Editer VScode \n\t 5. Flutter\nEXIT - ctrl+c\n\n";
 	read -p "Enter Your Option: " choice
@@ -30,26 +13,34 @@ while true; do
 
 		    ;;
 		2)
-		    sudo snap install vlc -y
+		    sudo snap install vlc
 			echo -e "\n\n VLC Media Install Competed......\n\n"
 		    ;;
 		3)
-		    	sudo apt install python3 -y
+		    	sudo apt install python3
 			echo -e "\n\n Python Install Competed......\n\n"
-			sudo apt install git -y
+			sudo apt install git
 			echo -e "\n\n GIT Install Competed......\n\n"
-			sudo apt install gcc -y
+			sudo apt install gcc
 			echo -e "\n\n GCC Install Competed......\n\n"
 		    ;;
 		4)
-			sudo snap install code --classic -y
+			sudo snap install code --classic
 			echo -e "\n\n VS Code Install Competed......\n\n"
 			;;
 		5)
-			sudo snap install code --classic -y
+			sudo snap install code --classic
 			read -p "Do you want to install Flutter? (y/n): " install_flutter
 				if [[ $install_flutter == "y" ]]; then
-				FLutter_Install  # Call the Flutter Install Function.
+				sudo snap install flutter --classic 
+				flutter sdk-path
+				flutter doctor
+				read -p "Do you want to Setup Flutter Path? (y/n): " flutter_path
+				if [[ $flutter_path == "y" ]]; then
+					export PATH="$PATH:/snap/bin/flutter/bin"
+					echo -e "\n\n Flutter Path Setup Competed......\n\n"
+				fi
+				echo -e "\n\n Flutter Install Competed......\n\n"
 			fi
 			;;
 		*)
@@ -57,4 +48,3 @@ while true; do
 		    ;;
 	esac
 done
-
